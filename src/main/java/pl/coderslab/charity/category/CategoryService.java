252ec6c6a0dc78a94fb8c14ce9e00054df0da216
PrzemyslawSolski <psolski@poczta.onet.pl>
2019-11-20 21:24:38 +0100

    @Autowired

--- src/main/java/pl/coderslab/charity/category/CategoryService.java
+++ src/main/java/pl/coderslab/charity/category/CategoryService.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Service
 @Transactional
 public class CategoryService {
+    
     private final CategoryRepository categoryRepository;
 
     @Autowired
--- src/main/java/pl/coderslab/charity/institution/InstitutionService.java
+++ src/main/java/pl/coderslab/charity/institution/InstitutionService.java
@@ -1,5 +1,6 @@
 package pl.coderslab.charity.institution;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +12,7 @@ public class InstitutionService {
 
     private final InstitutionRepository institutionRepository;
 
+    @Autowired
     public InstitutionService(InstitutionRepository institutionRepository) {
         this.institutionRepository = institutionRepository;
     }

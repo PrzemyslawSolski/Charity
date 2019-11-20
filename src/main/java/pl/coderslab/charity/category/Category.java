ff01aa00c080bc6a0dfb43736e76f83907e43ee3
PrzemyslawSolski <psolski@poczta.onet.pl>
2019-11-20 20:33:25 +0100

    Category - entity, repository and service

---
+++ src/main/java/pl/coderslab/charity/category/Category.java
@@ -0,0 +1,28 @@
+package pl.coderslab.charity.category;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/pl/coderslab/charity/category/CategoryService.java
@@ -0,0 +1,32 @@
+package pl.coderslab.charity.category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public void save (Category category){
+        categoryRepository.save(category);
+    }
+
+    public Category getOne (long id){
+        return categoryRepository.findById(id).orElse(null);
+    }
+
+    public List<Category> categories(){
+        return categoryRepository.findAll();
+    }
+
+    public void deleteById(long id){
+        categoryRepository.deleteById(id);
+    }
+
+
+}

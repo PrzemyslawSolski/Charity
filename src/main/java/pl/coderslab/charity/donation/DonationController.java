4c76709ce88df683af26de59d0b586f62d0ba60d
PrzemyslawSolski <psolski@poczta.onet.pl>
2019-11-22 21:32:21 +0100

    categories sent to view

--- src/main/java/pl/coderslab/charity/donation/DonationController.java
+++ src/main/java/pl/coderslab/charity/donation/DonationController.java
@@ -3,12 +3,19 @@ package pl.coderslab.charity.donation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.coderslab.charity.category.CategoryService;
 
 @Controller
 public class DonationController {
+    private final CategoryService categoryService;
+
+    public DonationController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("/donate")
     public String donateAction(Model model){
+        model.addAttribute("categories", categoryService.findAll());
         return "form";
     }
 }
